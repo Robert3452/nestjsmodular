e2e05049d1dbc6e83c0d90938269c4b005ed5684
Robert3452 <romeza1239@gmail.com>
2021-11-29 11:49:28 -0500

    [Feature]: Adding nest g gu path/apiKeys --flat and module guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiKeyGuard } from 'src/auth/guards/api-key.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @UseGuards(ApiKeyGuard)
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,11 +1,22 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
-
+import { Request } from 'express';
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Auth');
+    const isAuth = authHeader === '1234';
+    if (!isAuth) {
+      throw new UnauthorizedException('Api key is invalid');
+    }
+    return isAuth;
   }
 }
